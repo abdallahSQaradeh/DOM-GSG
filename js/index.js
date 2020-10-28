c265332095f14af29c759eb70970f3c97bb47c6d
Abdallah Qaradeh <abdallahsqaradeh@gmail.com>
2020-10-28 13:36:45 +0300

    finish editing index.js that modify the DOM

--- js/index.js
+++ js/index.js
@@ -43,3 +43,54 @@ const siteContent = {
 };
 
 // write your code here
+// ?Header 
+const nav = document.getElementsByTagName("nav");
+const headerImg = document.getElementById("logo-img")
+const navKeys = Object.keys(siteContent.nav);
+let i=0;
+for(;i<nav[0].children.length;i++)
+{
+  nav[0].children[i].textContent=siteContent.nav[navKeys[i]];
+
+}
+headerImg.src=siteContent.nav[navKeys[i]];
+//? cta
+const cta_div = document.getElementsByClassName("cta-text");
+cta_div[0].children[0].textContent = siteContent.cta.h1;
+cta_div[0].children[1].textContent= siteContent.cta.button;
+const cta_img = document.getElementById("cta-img");
+cta_img.src=siteContent.cta["img-src"];
+
+//? main content
+const top_content = document.getElementsByClassName("top-content")
+top_content[0].children[0].children[0].textContent  = siteContent["main-content"]["features-h4"];
+top_content[0].children[0].children[1].textContent  = siteContent["main-content"]["features-content"]
+
+top_content[0].children[1].children[0].textContent  = siteContent["main-content"]["about-h4"]
+top_content[0].children[1].children[1].textContent  = siteContent["main-content"]["about-content"]
+const middle_img = document.getElementById("middle-img");
+middle_img.src=siteContent["main-content"]["middle-img-src"];
+
+
+
+const bottom_content = document.getElementsByClassName("bottom-content")
+bottom_content[0].children[0].children[0].textContent  = siteContent["main-content"]["services-h4"]
+bottom_content[0].children[0].children[1].textContent  = siteContent["main-content"]["services-content"]
+
+bottom_content[0].children[1].children[0].textContent  = siteContent["main-content"]["product-h4"]
+bottom_content[0].children[1].children[1].textContent  = siteContent["main-content"]["product-content"]
+
+bottom_content[0].children[2].children[0].textContent  = siteContent["main-content"]["vision-h4"]
+bottom_content[0].children[2].children[1].textContent  = siteContent["main-content"]["vision-content"]
+
+// ? contact
+
+const contact = document.getElementsByClassName("contact")[0];
+contact.children[0].textContent  =siteContent.contact["contact-h4"]
+contact.children[1].textContent  =siteContent.contact.address
+contact.children[2].textContent  =siteContent.contact.phone
+contact.children[3].textContent  =siteContent.contact.email
+
+// ? footer
+const footer = document.getElementsByTagName("footer")[0];
+footer.children[0].textContent = siteContent.footer.copyright;
